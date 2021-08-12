11cf27f4dd7d2c64b362e850722b97aa2aabb7e0
Simon Mukunga <mmukunga@gmail.com>
2021-08-12 08:45:48 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './Main';
+import Main from './components/Main';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
